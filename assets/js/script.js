0344b170885ffb4caac6fcf64c4384c6c6d7fdab
firstnamejonas <firstname.jonas@icloud.com>
2024-02-16 14:09:10 +0100

    Optimize navbar to prevent horizontal scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,13 @@
-//------------------------------------------------------------------------------------------------- Open & Close Navbar Responsive:
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar')
+document.addEventListener("DOMContentLoaded", function() {
+    const navCheck = document.getElementById("nav-check");
+    const navLinks = document.querySelectorAll(".nav-links a");
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
-//------------------------------------------------------------------------------------------------- Active Links:
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+    navLinks.forEach(function(link) {
+      link.addEventListener("click", function() {
+        // Überprüfe, ob das Menü geöffnet ist, und schließe es bei Bedarf
+        if (navCheck.checked) {
+          navCheck.checked = false;
         }
+      });
     });
-
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    //remove navbar when link clicked
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+  });
